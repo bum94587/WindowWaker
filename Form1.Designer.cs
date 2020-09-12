c04280c6e2e0f74361e250e6575822272655cbd5
Bumshik Rob Pak <bum94587@yahoo.com>
2020-09-11 17:24:45 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WindowWaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Location = new System.Drawing.Point(12, 53);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(233, 43);
+            this.buttonGo.TabIndex = 0;
+            this.buttonGo.Text = "Start";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.ButtonGo_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(12, 102);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(233, 46);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.ButtonStop_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 187);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(286, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel1.Text = "Ready";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(106, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Notepad";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Process Name:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 154);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(105, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Capture Window";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 24);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Capture Now";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 209);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonGo);
+            this.Name = "Form1";
+            this.Text = "WindowsWaker";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using System.Timers;
+
+namespace WindowWaker
+{
+    public partial class Form1 : Form
+    {
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Rect
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        [FlagsAttribute]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+            // Legacy flag, should not be used.
+            // ES_USER_PRESENT = 0x00000004
+        }
+
+        [DllImport("user32.dll")]
+        private static extern int SetForegroundWindow(IntPtr hWnd);
+
+        private const int SW_RESTORE = 9;
+        private System.Timers.Timer myTimer;
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonGo_Click(object sender, EventArgs e)
+        {
+            SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
+            toolStripStatusLabel1.Text = "App Started: " + (DateTime.Now).ToString("h:mm:ss tt");
+
+            // Create a timer
+            myTimer = new System.Timers.Timer();
+
+            if (checkBox1.Checked)
+            {
+                // Tell the timer what to do when it elapses
+                myTimer.Elapsed += new ElapsedEventHandler(myEvent);
+                // Set it to go off every ten seconds
+                myTimer.Interval = 10000;
+                // And start it        
+                myTimer.Enabled = true;
+            }
+        }
+
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+            myTimer.Enabled = false;
+            toolStripStatusLabel1.Text = "App Stopped." + (DateTime.Now).ToString("h:mm:ss tt");
+        }
+
+        private void CaptureScreen(string procName)
+        {
+            Process proc;
+
+            try
+            {
+                proc = Process.GetProcessesByName(procName)[0];
+
+                // You need to focus on the application
+                SetForegroundWindow(proc.MainWindowHandle);
+                ShowWindow(proc.MainWindowHandle, SW_RESTORE);
+
+                // You need some amount of delay, but 1 second may be overkill
+                System.Threading.Thread.Sleep(1000);
+
+                Rect rect = new Rect();
+                IntPtr error = GetWindowRect(proc.MainWindowHandle, ref rect);
+
+                // sometimes it gives error.
+                while (error == (IntPtr)0)
+                {
+                    error = GetWindowRect(proc.MainWindowHandle, ref rect);
+                }
+
+                int width = rect.right - rect.left;
+                int height = rect.bottom - rect.top;
+
+                Bitmap bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+                Graphics.FromImage(bmp).CopyFromScreen(rect.left,
+                                                       rect.top,
+                                                       0,
+                                                       0,
+                                                       new Size(width, height),
+                                                       CopyPixelOperation.SourceCopy);
+
+
+                bmp.Save("c:\\tmp\\ScreenCapture_" + (DateTime.Now).ToString("mmddyy_hhmmsstt") + ".png", ImageFormat.Png);
+                //bmp.Save("c:\\tmp\\ScreenCapture" + ".png", ImageFormat.Png);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0} Exception caught.", e);
+            }
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            CaptureScreen(textBox1.Text);
+        }
+
+        // Implement a call with the right signature for events going off
+        private void myEvent(object source, ElapsedEventArgs e) {
+
+            if (checkBox1.Checked)
+            {
+                CaptureScreen(textBox1.Text);
+            }
+
+        }
+    }
+}
